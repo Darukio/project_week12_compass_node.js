44a02d4b87aef0bf9d652e2a81d727e06002faf3
Paulo SS <Damian-200@live.com.ar>
2022-12-17 15:39:29 -0300

    feature: add compression in app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const compression = require('compression')
 
 const eventRoutes = require('./routes/eventRoutes')
 const userRoutes = require('./routes/userRoutes')
 
 const app = express()
 app.use(express.json())
+
+app.use(compression())
 
 app.use('/api/v1/events', eventRoutes)
 app.use('/api/v1/users', userRoutes)
