2529d94d628817d3ebb535c9de4b9a15332e37a1
Paulo SS <Damian-200@live.com.ar>
2022-12-18 00:24:36 -0300

    feature: add helmet, rateLimit, mongoSanitize and xssClean

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const compression = require('compression')
-
+// eslint-disable-next-line node/no-extraneous-require
+const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
+const mongoSanitize = require('express-mongo-sanitize')
+const xssClean = require('xss-clean')
 const eventRoutes = require('./routes/eventRoutes')
 const userRoutes = require('./routes/userRoutes')
 
 const app = express()
-app.use(express.json())
+
+app.use(helmet())
+
+const limiter = rateLimit({
+	max: 100,
+	windowMs: 60 * 60 * 1000,
+	message: 'Too many requests from this IP, please try again in an hour',
+})
+app.use(limiter)
+
+app.use(express.json({ limit: '10kb' }))
+
+app.use(mongoSanitize())
+app.use(xssClean())
 
 app.use(compression())
 
