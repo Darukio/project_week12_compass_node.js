464209e386e7a54cdc043d45b0b978f87d0674c7
Paulo SS <Damian-200@live.com.ar>
2023-01-12 07:21:56 -0300

    fix: eliminate createdAt property because it appears in mongoose timestamps

--- models/eventModel.js
+++ models/eventModel.js
@@ -1,21 +1,17 @@
 const mongoose = require('mongoose')
 
 const eventSchema = new mongoose.Schema(
-	{
-		description: {
-			required: [true, 'An Event must have a description'],
-			type: String,
-		},
-		dateTime: {
-			required: [true, 'An Event must have a date'],
-			type: Date,
-		},
-		createdAt: {
-			type: Date,
-			default: Date.now,
-		},
-	},
-	{ timestamps: true }
+    {
+        description: {
+            required: [true, 'An Event must have a description'],
+            type: String,
+        },
+        dateTime: {
+            required: [true, 'An Event must have a date'],
+            type: Date,
+        },
+    },
+    { timestamps: true }
 )
 
 module.exports = mongoose.model('Event', eventSchema)
